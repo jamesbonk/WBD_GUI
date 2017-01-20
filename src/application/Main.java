a7e8b58548177e12029bb6b4fbd7b2c6318277d6
jamesbonk <patryk.bak@gazeta.pl>
2017-01-20 22:15:50 +0100

    gui + db connection

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,52 @@
 package application;
-	
+
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-
+import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
+
+	private Stage primaryStage;
+	private AnchorPane rootLayout;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			this.primaryStage = primaryStage;
+	        this.primaryStage.setTitle("Agency Management");
+			 initRootLayout();
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	 public void initRootLayout() {
+	        try {
+	            // Load root layout from fxml file.
+	            FXMLLoader loader = new FXMLLoader();
+	            loader.setLocation(Main.class.getResource("../RootLayout.fxml"));
+	            rootLayout = (AnchorPane) loader.load();
+
+	            // Show the scene containing the root layout.
+	            Scene scene = new Scene(rootLayout);
+	            primaryStage.setScene(scene);
+	            primaryStage.show();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	 
+	 public Stage getPrimaryStage() {
+	        return primaryStage;
+	    }
+
 	public static void main(String[] args) {
 		launch(args);
 	}
