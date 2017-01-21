e8ebe0f31e1a33a2e047d34ef4bd50b9d7b4ae23
Tomek <motyka.tomasz.1995@gmail.com>
2017-01-21 01:21:27 +0100

    adding controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -39,7 +39,7 @@ public class Main extends Application {
 	            primaryStage.setScene(scene);
 	            primaryStage.show();
 	        } catch (IOException e) {
-	            e.printStackTrace();
+				e.printStackTrace();
 	        }
 	    }
 	 
