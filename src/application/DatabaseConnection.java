b84cefcf65033df327bec0db95bbb6b31935db16
jamesbonk <patryk.bak@gazeta.pl>
2017-01-23 22:37:52 +0100

    controllers + table

--- src/application/DatabaseConnection.java
+++ src/application/DatabaseConnection.java
@@ -1,38 +1,63 @@
 package application;
 
-import java.beans.Statement;
+import java.sql.Statement;
+import java.sql.Types;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import controller.Main;
+import model.Negotiation;
+import oracle.jdbc.internal.OracleTypes;
 
 public class DatabaseConnection {
-	
-	public DatabaseConnection(){
-		
+
+	private Main mainApp;
+
+	public DatabaseConnection() {
+
 	}
 
+	public void loadProperties(Negotiation n) {
+		Statement stmt = null;
+		String query = "{call update_NIERUCHOMOSCI(2)}";
+		PreparedStatement callStmt = null;
+
+		try {
+			//this.mainApp.getConnection().prepareCall( query);
+
+			 callStmt = this.mainApp.getConnection().prepareCall(query);
+			 //callStmt.setInt(1,n.getID_BUDYNKU());
+			 callStmt.executeQuery();
+			//stmt.executeQuery(query);
+
+		} catch (java.sql.SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void setMain(Main mainApp) {
+		this.mainApp = mainApp;
+	}
 
 	public Connection GetConnection() {
 		Connection connection = null;
 
-		try
-		{
+		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-		}
-		catch(ClassNotFoundException e)
-		{
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 			return null;
 		}
 
-		try
-		{
-			connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "Motykson", "siekiera15");
+		try {
+			connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "jamesbonk", "tona101");
 
-		}
-		catch(SQLException e)
-		{
+		} catch (SQLException e) {
 			System.out.println("Connection failed");
 			e.printStackTrace();
 			return null;
--- src/view/OffersController.java
+++ src/view/OffersController.java
@@ -1,4 +1,4 @@
-package application;
+package view;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,6 +11,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
+import model.Negotiation;
 
 import java.sql.PreparedStatement;
 import java.sql.Statement;
@@ -21,18 +22,25 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import application.DatabaseConnection;
+import controller.Main;
 
 /**
  * Created by Tomasz Motyka on 2017-01-20.
  */
-public class Controller {
+public class OffersController {
 
 
     private DatabaseConnection dbConnection;
+    private Main mainApp;
     Connection connection;
+
 
     @FXML private ComboBox<String> preferencjeSelectBox;
     @FXML private ChoiceBox<String> klientSelectBox;
@@ -48,43 +56,19 @@ public class Controller {
     @FXML private CheckBox pokoje_do;
 
 
-    @FXML
-    private TableView negocjacjeTable = new TableView();
-    @FXML
-    TableColumn idBudynkuColumn = new TableColumn("ID BUDYNKU");
-    @FXML
-    TableColumn deweloperColumn = new TableColumn("DEWELOPER");
-    @FXML
-    TableColumn pracownikColumn = new TableColumn("PRACOWNIK");
-    @FXML
-    TableColumn stanColumn = new TableColumn("STAN");
-    @FXML
-    TableColumn typColumn = new TableColumn("TYP");
-    @FXML
-    TableColumn iloscColumn = new TableColumn("ILOSC");
-    @FXML
-    TableColumn powColumn = new TableColumn("POW.");
-    @FXML
-    TableColumn miastoColumn = new TableColumn("MIASTO");
-    @FXML
-    TableColumn ulicaColumn = new TableColumn("ULICA");
-    @FXML
-    TableColumn nrBudynkuColumn = new TableColumn("NR. BUDYNKU");
 
-    public Controller() {
 
-        this.dbConnection = new DatabaseConnection();
-        this.connection = dbConnection.GetConnection();
-        negocjacjeTable.setEditable(true);
-        negocjacjeTable.getColumns().addAll(idBudynkuColumn, deweloperColumn, pracownikColumn,stanColumn,typColumn,iloscColumn,powColumn,miastoColumn,ulicaColumn,nrBudynkuColumn);
-        idBudynkuColumn.setCellValueFactory(new PropertyValueFactory<>("ID_BUDYNKU"));
-        deweloperColumn.setCellFactory(new PropertyValueFactory<>("DEWELOPER"));
+    public OffersController() {
+    	 this.dbConnection = new DatabaseConnection();
+         this.connection = dbConnection.GetConnection();
+
     }
 
 
     @FXML
     public void initialize()
     {
+    	
         Statement stmt = null;
         String query = "select IMIE, NAZWISKO from KLIENCI";
         DisablePreferences();
@@ -99,8 +83,7 @@ public class Controller {
         catch(java.sql.SQLException e){
             e.printStackTrace();
         }
-
-        fillNegotiationsTable();
+      
     }
 
     public int getClient()
@@ -254,143 +237,14 @@ public class Controller {
 
     }
 
-
-	private void fillNegotiationsTable() {
-		Statement stmt = null;
-		String query = "select  B.ID_BUDYNKU, D.NAZWA, (P.IMIE|| ' '|| P.NAZWISKO) AS PRACOWNIK, N.STAN,B.TYPNIERUCHOMOSCI,B.ILOSC,B.POWIERZCHNIA,B.MIASTO,B.ULICA,B.NR_BUDYNKU from BUDYNKI B, NEGOCJACJE N, PROJEKTY P, DEWELOPERZY D,PRACOWNICY P where N.ID_BUDYNKU = B.ID_BUDYNKU and N.ID_PRACOWNIKA = P.ID_PRACOWNIKA";
-		try {
-			stmt = this.connection.createStatement();
-			ResultSet rs = stmt.executeQuery(query);
-			ObservableList<Negotiation> data = FXCollections.observableArrayList();
-			negocjacjeTable.getColumns().clear();
-			while (rs.next()) {
-
-				//this.preferencjeSelectBox.getItems().addAll(rs.getNString("STAN"));
-				// + " " + rs.getString("NAZWA"));
-				data.add(new Negotiation(rs.getString("ID_BUDYNKU"),rs.getString("NAZWA"),rs.getString("PRACOWNIK"),rs.getString("STAN"),rs.getString("TYPNIERUCHOMOSCI"),rs.getString("ILOSC"),rs.getString("POWIERZCHNIA"),rs.getString("MIASTO"),rs.getString("ULICA"),rs.getString("NR_BUDYNKU")));
-//				allData.add(rs.getString("ID_BUDYNKU"));
-//				allData.add(rs.getString("NAZWA"));
-//				allData.add(rs.getString("PRACOWNIK"));
-//				allData.add(rs.getString("STAN"));
-//				allData.add(rs.getString("TYPNIERUCHOMOSCI"));
-//				allData.add(rs.getString("ILOSC"));
-//				allData.add(rs.getString("POWIERZCHNIA"));
-//				allData.add(rs.getString("MIASTO"));
-//				allData.add(rs.getString("ULICA"));
-//				allData.add(rs.getString("NR_BUDYNKU"));
-			}
-			negocjacjeTable.setItems(data);
-			negocjacjeTable.getColumns().addAll(idBudynkuColumn,deweloperColumn,pracownikColumn,stanColumn,typColumn,iloscColumn,powColumn,miastoColumn,ulicaColumn,nrBudynkuColumn);
-			
-		} catch (java.sql.SQLException e) {
-			e.printStackTrace();
-		}
-
+	
+	public void setMain(Main main){
+		this.mainApp = main;
 	}
 	
-	public static class Negotiation{
-		private final SimpleStringProperty ID_BUDYNKU;
-		private final SimpleStringProperty DEWELOPER;
-		private final SimpleStringProperty PRACOWNIK;
-		private final SimpleStringProperty STAN;
-		private final SimpleStringProperty TYPNIERUCHOMOSCI;
-		private final SimpleStringProperty ILOSC;
-		private final SimpleStringProperty POWIERZCHNIA;
-		private final SimpleStringProperty MIASTO;
-		private final SimpleStringProperty ULICA;
-		private final SimpleStringProperty NR_BUDYNKU;
-		
-		private Negotiation(String ID_BUDYNKU, String NAZWA, String PRACOWNIK, String STAN, String TYPNIERUCHOMOSCI, String ILOSC, String POWIERZCHNIA, String MIASTO, String ULICA, String NR_BUDYNKU){
-			this.ID_BUDYNKU = new SimpleStringProperty(ID_BUDYNKU);
-			this.DEWELOPER = new SimpleStringProperty(NAZWA);
-			this.PRACOWNIK = new SimpleStringProperty(PRACOWNIK);
-			this.STAN = new SimpleStringProperty(STAN);
-			this.TYPNIERUCHOMOSCI = new SimpleStringProperty(TYPNIERUCHOMOSCI);
-			this.ILOSC = new SimpleStringProperty(ILOSC);
-			this.POWIERZCHNIA = new SimpleStringProperty(POWIERZCHNIA);
-			this.MIASTO = new SimpleStringProperty(MIASTO);
-			this.ULICA= new SimpleStringProperty(ULICA);
-			this.NR_BUDYNKU = new SimpleStringProperty(NR_BUDYNKU);
-			
-		}
-
-		public void setID_BUDYNKU(String p) {
-			ID_BUDYNKU.set(p);
-		}
-		public void setDEWELOPER(String p) {
-			DEWELOPER.set(p);
-		}
-		public void setPRACOWNIK(String p) {
-			PRACOWNIK.set(p);
-		}
-		public void setSTAN(String p) {
-			STAN.set(p);
-		}
-
-		public void setTYPNIERUCHOMOSCI(String p) {
-			TYPNIERUCHOMOSCI.set(p);
-		}
-		public void setILOSC(String p) {
-			ILOSC.set(p);
-		}
-		
-		public void setPOWIERZCHNIA(String p) {
-			POWIERZCHNIA.set(p);
-		}
-		public void setMIASTO(String p) {
-			MIASTO.set(p);
-		}
-		public void setULICA(String p) {
-			ULICA.set(p);
-		}
-		public void setNR_BUDYNKU(String p) {
-			NR_BUDYNKU.set(p);;
-		}
-
-		
-		public String getID_BUDYNKU() {
-			return ID_BUDYNKU.get();
-		}
-
-		public String getDEWELOPER() {
-			return DEWELOPER.get();
-		}
-
-		public String getPRACOWNIK() {
-			return PRACOWNIK.get();
-		}
-
-		public String getSTAN() {
-			return STAN.get();
-		}
-
-		public String getTYPNIERUCHOMOSCI() {
-			return TYPNIERUCHOMOSCI.get();
-		}
-
-		public String getILOSC() {
-			return ILOSC.get();
-		}
-
-		public String getPOWIERZCHNIA() {
-			return POWIERZCHNIA.get();
-		}
-
-		public String getMIASTO() {
-			return MIASTO.get();
-		}
-
-		public String getULICA() {
-			return ULICA.get();
-		}
-
-		public String getNR_BUDYNKU() {
-			return NR_BUDYNKU.get();
-		}
-		
-		
-		
-	}
+	 public void setDbConnection(DatabaseConnection dbConn) {
+	        this.dbConnection = dbConn;
+	    }
 
 
 }
